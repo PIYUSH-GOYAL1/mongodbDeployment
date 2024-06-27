a2fc891dd4f387b16c0c03919ae3651f2f508638
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 17:05:00 +0530

    updated

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userModel = require("./models/userModel");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.set("view engine" , "ejs");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname , "public")));
 
 dbConnect();
