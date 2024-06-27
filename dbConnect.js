58bc5d0adb881f60f780713b97b8a702b31e4d9b
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 17:13:21 +0530

    update 2

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://piyushgoyal11824:6JtwzwQXWotEzvD1@testcluster.vgvzmvb.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster")
+        await mongoose.connect("mongodb+srv://piyushgoyal11824:6JtwzwQXWotEzvD1@testcluster.vgvzmvb.mongodb.net/build?retryWrites=true&w=majority&appName=TestCluster")
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
