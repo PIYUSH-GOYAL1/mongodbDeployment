bfd63c7ec5a3762b1bb72e346b20ff22e1341eed
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 18:52:56 +0530

    dot env file added

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const path = require("path");
+app.use(express.json());
 const dbConnect = require("./dbConnect");
+dbConnect();
 const userModel = require("./models/userModel");
 
-app.use(express.json());
+
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname , "public")));
 
-dbConnect();
 
 app.get("/" , (req,res)=>{
     try{
