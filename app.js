a89a9dcb80fe6a3d63513567c46818dbee92138d
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 16:50:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const dbConnect = require("./dbConnect");
+const userModel = require("./models/userModel");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname , "public")));
+
+dbConnect();
+
+app.get("/" , (req,res)=>{
+    try{
+        res.render("index");
+    }catch(err){
+        res.render("error" , {err});
+    }
+});
+
+app.get("/getform" , (req,res)=>{
+    try{
+        res.render("form");
+    }catch(err){
+        res.render("error" , {err});
+    }
+});
+
+app.post("/processform" , async (req,res)=>{
+    try{
+        const newUser = await userModel.create({
+            fullname : req.body.fullname,
+            email : req.body.email,
+            password : req.body.password,
+            age : req.body.password,
+        });
+        res.redirect("/");
+    }catch(err){
+        res.render("error" , {err});
+    }
+});
+
+app.listen(3000, ()=>{
+    console.log("App is running on PORT 3000");
+})
---
+++ dbConnect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://piyushgoyal11824:6JtwzwQXWotEzvD1@testcluster.vgvzmvb.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster")
+        console.log("Connect to MongoDB successfully")
+    } catch (error) {
+        console.log("Connect failed " + error.message )
+    }
+}
+
+module.exports = connectDB
