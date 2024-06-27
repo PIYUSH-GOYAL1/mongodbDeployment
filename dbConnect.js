e004712135bbee597123d28019e215e4600fa951
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 17:17:35 +0530

    update 3

--- dbConnect.js
+++ dbConnect.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://piyushgoyal11824:6JtwzwQXWotEzvD1@testcluster.vgvzmvb.mongodb.net/build?retryWrites=true&w=majority&appName=TestCluster")
-        console.log("Connect to MongoDB successfully")
+        await mongoose.connect("mongodb+srv://piyushgoyal11824:6JtwzwQXWotEzvD1@testcluster.vgvzmvb.mongodb.net/build?retryWrites=true&w=majority&appName=TestCluster", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB successfully");
     } catch (error) {
-        console.log("Connect failed " + error.message )
+        console.error("Connection failed: " + error.message);
     }
-}
+};
 
 module.exports = connectDB
