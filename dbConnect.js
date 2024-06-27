6464869e653563c9f799f56586eebf444e32a269
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-06-27 18:20:35 +0530

    newest update

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/testDeployment", {
+        await mongoose.connect("mongodb+srv://localhost/testDeployment", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
